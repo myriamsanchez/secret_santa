47604222e1f0c50f5ce96172a56cb6c6bc879bf4
nekoinverse <35851828+nekoinverse@users.noreply.github.com>
2018-11-09 09:45:00 +0100

    created class InputSecretSanta, added more tests for InputSecretSanta

--- tests/InputSecretSantaTest.php
+++ tests/InputSecretSantaTest.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Tests;
 
+require_once(__DIR__ . "/../src/InputSecretSanta.php");
+
 use PHPUnit\Framework\TestCase;
+use SecretSanta\InputSecretSanta;
 
 final class InputSecretSantaTest extends TestCase
 {
@@ -12,6 +15,22 @@ final class InputSecretSantaTest extends TestCase
         $this->assertTrue(
             method_exists(InputSecretSanta::class, 'read'),
             "Class doesn't have method read"
+        );
+    }
+
+    public function testReadInputDataIsCorrect()
+    {
+        $input = new InputSecretSanta();
+
+        $participants   = $input->read();
+        $expected       = array_unique($participants);
+
+        $this->assertTrue(
+            is_array($participants)
+        );
+
+        $this->assertTrue(
+            $participants === $expected
         );
     }
 }
