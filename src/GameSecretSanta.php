3c50128a232730e488e515e5a7c33d8f7c045cf4
nekoinverse <35851828+nekoinverse@users.noreply.github.com>
2018-11-09 18:50:04 +0100

    added class GameSecretSanta, added more tests for GameSecretSanta

---
+++ src/GameSecretSanta.php
@@ -0,0 +1,85 @@
+<?php
+namespace SecretSanta;
+
+require_once ('Contracts/Game.php');
+require_once ('InputSecretSanta.php');
+require_once ('InputSecretSanta.php');
+
+use SecretSanta\Contracts\Game;
+
+class GameSecretSanta implements Game
+{
+    public function play()
+    {
+        $inputObj       = new InputSecretSanta();
+        $participants   = $inputObj->read();
+
+        $partnersList   = $this->getPartnersList($participants);
+        $this->showSecretSantaList($partnersList);
+    }
+
+    public function prepareSecretSantaList($list) : array
+    {
+        $partners = [];
+
+        foreach ($list as $participant => $partner) {
+            $partners[] = $participant . " -> " . $partner;
+        }
+        return $partners;
+    }
+
+    protected function showSecretSantaList($list)
+    {
+        $outputObj  = new OutputSecretSanta();
+
+        $list = $this->prepareSecretSantaList($list);
+
+        foreach ($list as $line) {
+
+            $outputObj->printLine($line);
+        }
+    }
+
+    protected function getPossiblePartners(array $list, $participant): array
+    {
+        $id = array_search($participant, $list);
+        if ($id !== false) {
+            array_splice($list, $id, 1);
+        }
+        return $list;
+    }
+
+    protected function getPartnersList(array $list)
+    {
+        $partnersList           = [];
+        $unassignedParticipants = $list;
+        $randomObj              = new RandomSecretSanta();
+
+        foreach ($list as $participant) {
+            $possiblePartners = $this->getPossiblePartners($unassignedParticipants, $participant);
+
+            if (sizeof($possiblePartners) == 0) {
+                // there only unassigned partner is the current one
+                // switch partners assigned with the first participant
+                reset($partnersList);
+
+                $firstParticipant   = key($partnersList);
+                $firstAssigned      = reset($partnersList);
+
+                $partnersList[$firstParticipant]    = $participant;
+                $partnersList[$participant]         = $firstAssigned;
+            } else {
+                if (sizeof($possiblePartners) == 1) {
+                    $id = 0;
+                } else {
+                    $id = $randomObj->generate(0, (sizeof($possiblePartners)-1));
+                }
+                $partnersList[$participant] = $possiblePartners[$id];
+
+                $assignedParner = array_search($possiblePartners[$id], $unassignedParticipants);
+                array_splice($unassignedParticipants, $assignedParner, 1);
+            }
+        }
+        return $partnersList;
+    }
+}
--- tests/GameSecretSantaTest.php
+++ tests/GameSecretSantaTest.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Tests;
 
+require_once(__DIR__ . "/../src/GameSecretSanta.php");
+
 use PHPUnit\Framework\TestCase;
+use SecretSanta\GameSecretSanta;
 
 final class GameSecretSantaTest extends TestCase
 {
@@ -12,6 +15,25 @@ final class GameSecretSantaTest extends TestCase
         $this->assertTrue(
             method_exists(GameSecretSanta::class, 'play'),
             "Class doesn't have method play"
+        );
+    }
+
+    public function testLisHasExpectedFormat(): void
+    {
+        $game = new GameSecretSanta();
+
+        $list = [
+            'Juan' => 'Luisa',
+            'Luisa' => 'Pedro'
+        ];
+
+        $receivedList = $game->prepareSecretSantaList($list);
+        $expectedOutput = [
+            'Juan -> Luisa',
+            'Luisa -> Pedro'
+        ];
+        $this->assertTrue(
+            $receivedList === $expectedOutput
         );
     }
 }
